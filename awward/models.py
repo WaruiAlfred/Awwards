a70b528b58daf4ee73a7b5d0e342a201f352bf75
Alfred Warui <alfredwarui17@gmail.com>
2021-10-22 15:32:14 +0300

    create projects model

--- awward/models.py
+++ awward/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+#Projects model
+class Projects(models.Model): 
+  project_owner = models.ForeignKey(User,on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  description = models.TextField()
+  screenshot = CloudinaryField('project')
+  live_link = models.URLField(max_length=100)
+  date_added = models.DateField(auto_now_add=True)
+  
+  def __str__(self):
+    return self.name 
+  
+  
