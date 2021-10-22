ae398a0e939c98706de3c56a80587a3584504dae
Alfred Warui <alfredwarui17@gmail.com>
2021-10-22 18:00:06 +0300

    add awward app urls file

---
+++ awward/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+#awward app urls
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('profile/',views.profile,name='profile'),
+    path('profile/update/',views.update_profile,name='profileUpdate')
+]
