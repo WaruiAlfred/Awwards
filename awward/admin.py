dc8da7dd798887c3d2fe84f25cc9ffb75e2a1a15
Alfred Warui <alfredwarui17@gmail.com>
2021-10-23 23:01:55 +0300

    register Projects and Profile models in django admin

--- awward/admin.py
+++ awward/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Projects,Profile
 
-# Register your models here.
+#Registering models
+admin.site.register(Projects)
+admin.site.register(Profile)
