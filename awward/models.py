6006062795809a93628f4170c593a9d3669d2fb0
Alfred Warui <alfredwarui17@gmail.com>
2021-10-23 22:59:17 +0300

    create profile model

--- awward/models.py
+++ awward/models.py
@@ -6,12 +6,26 @@ from cloudinary.models import CloudinaryField
 class Projects(models.Model): 
   project_owner = models.ForeignKey(User,on_delete=models.CASCADE)
   name = models.CharField(max_length=100)
-  description = models.TextField()
-  screenshot = CloudinaryField('project')
+  description = models.TextField(max_length=400)
+  screenshot = CloudinaryField('screenshot')
   live_link = models.URLField(max_length=100)
   date_added = models.DateField(auto_now_add=True)
   
   def __str__(self):
     return self.name 
   
+#Profile model
+class Profile(models.Model): 
+  user = models.OneToOneField(User,on_delete=models.CASCADE)
+  bio = models.TextField(max_length=400)
+  country = models.CharField(max_length=30)
+  phone_number = models.CharField(max_length=10)
+  picture = CloudinaryField('profile')
+  
+  def save_profile(self): 
+    '''Funtion to save a profile object'''
+    self.save()
+  
+  def __str__(self):
+    return self.user.username
   
