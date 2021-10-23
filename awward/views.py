741a1e8147328785aa42dfee6f7be6acfe87dd71
Alfred Warui <alfredwarui17@gmail.com>
2021-10-23 22:58:40 +0300

    create view for project post form in home function

--- awward/views.py
+++ awward/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import ProfileUpdateForm,UserUpdateForm,ProjectAddForm
+from django.contrib import messages
+from .models import Profile,Projects
 
-# Create your views here.
+#View functions
+def home(request): 
+  '''Function rendering the home page'''
+  
+  projects = Projects.objects.all() #obtaining all posted projects
+  
+  if request.method == 'POST': 
+    form = ProjectAddForm(request.POST,request.FILES)
+    if form.is_valid(): 
+      project=form.save(commit=False)
+      project.project_owner = request.user 
+      project.save()
+      messages.success(request,'Your project has been successfully posted!')
+      return redirect('home')
+  else: 
+    form = ProjectAddForm()
+
+  return render(request,'index.html',{"form":form,"projects":projects})
+
+def profile(request): 
+  '''Function rendering a logged in user's profile page'''
+  profile = Profile.objects.filter(user=request.user).first()
+  return render(request,'registration/profile.html',{"profile":profile})
+
+def update_profile(request): 
+  '''Function for user profile update'''
+  profile = Profile.objects.filter(user=request.user).first()
+  if request.method == 'POST': 
+    u_form = UserUpdateForm(request.POST,instance=request.user)
+    p_form = ProfileUpdateForm(request.POST,request.FILES,instance=profile)
+    if u_form.is_valid() and p_form.is_valid(): 
+      u_form.save()
+      user_profile=p_form.save(commit=False)
+      user_profile.user = request.user 
+      user_profile.save()
+      messages.success(request,'Your account has been updated!')
+      return redirect('profile')
+  else: 
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=profile)
+  
+  context = {
+    'u_form': u_form,
+    'p_form': p_form
+  }
+  return render(request,'registration/update_profile.html',context)
+
+# def project(request): 
+#   '''Function handling post project form'''
+#   return render(request,'projects/project.html')
