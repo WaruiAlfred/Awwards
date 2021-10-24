e9af3934ff1859a8e43c991d6af08f924bd48346
Alfred Warui <alfredwarui17@gmail.com>
2021-10-24 13:02:55 +0300

    create project rating form

--- awward/forms.py
+++ awward/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Profile,Projects
+from .models import Profile,Projects,Ratings,RATE_CHOICES
 from django.contrib.auth.models import User
 
 #Profile form
@@ -18,4 +18,15 @@ class UserUpdateForm(forms.ModelForm):
 class ProjectAddForm(forms.ModelForm): 
   class Meta: 
     model = Projects
-    fields = ['name','description','live_link','screenshot']
+    fields = ['name','description','main_technology_used','live_link','screenshot']
+    
+#Rating form
+class ProjectRatingForm(forms.ModelForm): 
+  review = forms.CharField(widget=forms.Textarea(),required=False)
+  design_rate = forms.ChoiceField(choices=RATE_CHOICES,widget=forms.Select(),required=True)
+  usability_rate = forms.ChoiceField(choices=RATE_CHOICES,widget=forms.Select(),required=True)
+  content_rate = forms.ChoiceField(choices=RATE_CHOICES,widget=forms.Select(),required=True)
+ 
+  class Meta: 
+    model = Ratings
+    fields = ('design_rate','usability_rate','content_rate','review') 
