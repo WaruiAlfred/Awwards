394b758c16aface6e5f3f8c1b8c6b4d8a0fbea98
Alfred Warui <alfredwarui17@gmail.com>
2021-10-24 13:03:49 +0300

    create ratings model and register it in admin

--- awward/admin.py
+++ awward/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Projects,Profile
+from .models import Projects,Profile,Ratings
 
 #Registering models
 admin.site.register(Projects)
 admin.site.register(Profile)
+admin.site.register(Ratings)
--- awward/models.py
+++ awward/models.py
@@ -7,6 +7,7 @@ class Projects(models.Model):
   project_owner = models.ForeignKey(User,on_delete=models.CASCADE)
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=400)
+  main_technology_used = models.CharField(max_length=30,null=True)
   screenshot = CloudinaryField('screenshot')
   live_link = models.URLField(max_length=100)
   date_added = models.DateField(auto_now_add=True)
@@ -28,4 +29,30 @@ class Profile(models.Model):
   
   def __str__(self):
     return self.user.username
+
+#Rate/Review model
+
+RATE_CHOICES = [
+  (1, '1 - Unlikeable'),
+  (2, '2 - Horrible'),
+  (3, '3 - Hmmmm'),
+  (4, '4 - Bad'),
+  (5, '5 - Fine'),
+  (6, '6 - Good'),
+  (7, '7 - Very Good'),
+  (8, '8 - Likeable'),
+  (9, '9 - Perfect'),
+  (10, '10 - Awesome'),
+]
+
+class Ratings(models.Model): 
+  user = models.ForeignKey(User,on_delete=models.CASCADE)  
+  project = models.ForeignKey(Projects,on_delete=models.CASCADE)  
+  design_rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+  usability_rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+  content_rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+  review = models.TextField(max_length=1000,blank=True)
+  date = models.DateField(auto_now_add=True)
   
+  def __str__(self):
+    return self.project.name
